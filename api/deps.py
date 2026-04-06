06aaabd710f64945225476689f8a7271cb35f26e
radiaated <iamsumankht@gmail.com>
2026-04-06 11:09:23 +0545

    feat: add /api/user/profile/ endpoint

--- api/deps.py
+++ api/deps.py
@@ -1,4 +1,11 @@
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt
+
 from db.session import Session
+from core.config import settings
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/auth/login/")
 
 
 def get_db():
@@ -9,3 +16,21 @@ def get_db():
         yield session
     finally:
         session.close()
+
+
+def get_auth_user(token: str = Depends(oauth2_scheme)):
+
+    try:
+
+        payload = jwt.decode(token, settings.SECRET_KEY, [settings.JWT_ALGORITHM])
+        user_id = payload.get("sub")
+
+        if not user_id:
+
+            raise HTTPException(status_code=402, detail="Invalid token")
+
+        return int(user_id)
+
+    except:
+
+        raise HTTPException(status_code=402, detail="Invalid token")
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
 
 from api.endpoints.auth import auth_router
+from api.endpoints.user import user_router
 
 import db.init_db
 
 app = FastAPI()
 
 app.include_router(auth_router, prefix="/api/auth")
+app.include_router(user_router, prefix="/api/user")
 
 
 @app.get("/ping/")
