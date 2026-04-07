70a778db58328dc7999a2eb94b76b60a38a766f0
radiaated <iamsumankht@gmail.com>
2026-04-07 12:47:08 +0545

    refactor: update status code from status module

--- api/deps.py
+++ api/deps.py
@@ -1,4 +1,4 @@
-from fastapi import Depends, HTTPException
+from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from jose import jwt, JWTError, ExpiredSignatureError
 
@@ -31,19 +31,27 @@ def get_auth_user(token: str = Depends(oauth2_scheme)):
         user_role = payload.get("role")
 
         if not user_id or not user_role:
-            raise HTTPException(status_code=401, detail="Invalid token")
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token"
+            )
 
         return user_id, user_role
 
     except ExpiredSignatureError:
-        raise HTTPException(status_code=401, detail="Token has expired")
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Token has expired"
+        )
 
     except JWTError:
-        raise HTTPException(status_code=401, detail="Invalid token")
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token"
+        )
 
     except Exception as ex:
         print(ex)
-        raise HTTPException(status_code=401, detail="Invalid token")
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token"
+        )
 
 
 def role_check(*args: List[str]):
@@ -55,7 +63,9 @@ def role_check(*args: List[str]):
 
     def role_checker(auth_user: dict = Depends(get_auth_user)):
         if auth_user[1] not in args:
-            raise HTTPException(status_code=403, detail="Forbidden")
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN, detail="Forbidden"
+            )
         return int(auth_user[0])
 
     return role_checker
